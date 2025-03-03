c654b123b627d4cdd2b7dd1d2ee87aa50bfc66bf
Hernán Valdés <hvaldes@avantgarde-labs.de>
2025-03-03 14:56:12 +0100

    Add basic db model for links

---
+++ app/infrastructure/database/models/link.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.infrastructure.database.models.base import Base
+
+
+class Link(Base):
+    __tablename__ = "link"
+    __table_args__ = {"schema": "link_shortener"}
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    url: Mapped[str]
+    short_url: Mapped[str] = mapped_column(String, index=True)
+    created_at: Mapped[datetime]
+    live_until: Mapped[datetime | None]
