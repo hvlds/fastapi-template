8065bf442e14ab6566c7e8ff2afc9c4516bd3a5c
Hernán Valdés <hvaldes@avantgarde-labs.de>
2025-03-03 15:54:32 +0100

    Add a basic repository for the Database Links

---
+++ app/infrastructure/repositories/link_repository.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.infrastructure.database.models.link import Link
+from app.infrastructure.database.utils import get_db
+
+
+class LinkRepository:
+    def __init__(self, db_session: Annotated[Session, Depends(get_db)]):
+        self.session = db_session
+
+    def get_links(self) -> list[Link]:
+        stmt = select(Link)
+        result = self.session.execute(stmt).scalars().all()
+        return [link for link in result]
