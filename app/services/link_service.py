c2b52835cd15178877599a1b69861193748965cb
Hernán Valdés <hvaldes@avantgarde-labs.de>
2025-03-03 18:42:37 +0100

    Generate random base62 shortened URL using uuid4 as a seed

--- app/services/link_service.py
+++ app/services/link_service.py
@@ -1,3 +1,5 @@
+import string
+import random
 from typing import Annotated
 from uuid import uuid4
 
@@ -22,7 +24,14 @@ class LinkService:
     def create_link(self, new_link: CreateLinkApi) -> LinkApi:
         db_link = self.link_repository.create_link(
             url=new_link.url.unicode_string(),
-            short_url=str(uuid4()),
+            short_url=LinkService.generate_short_id(seed=str(uuid4())),
             live_until=new_link.live_until,
         )
         return LinkApiMapper.map(db_link)
+
+    @staticmethod
+    def generate_short_id(seed: str, length=6):
+        """Generate a random short ID of specified length using Base62 encoding."""
+        random.seed(seed)
+        base62_chars = string.digits + string.ascii_letters
+        return "".join(random.choice(base62_chars) for _ in range(length))
